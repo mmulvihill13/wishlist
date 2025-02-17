9a48fbcb16bece0c3cfc7914490a45a986bffaab
ana-gilbert <152214588+ana-gilbert@users.noreply.github.com>
2025-02-16 21:04:32 -0500

    saving local changes before merge

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'home/static'),
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
