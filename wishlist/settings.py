43bb1421ca8b8eab8331f4c7da106471fe6bf4fc
ana-gilbert <152214588+ana-gilbert@users.noreply.github.com>
2025-03-13 17:01:38 -0400

    static files and settings.py

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collected static files go
+STATIC_ROOT = BASE_DIR / 'static'  # Where collected static files go
 
 # Remove STATICFILES_DIRS in production
 if DEBUG:
