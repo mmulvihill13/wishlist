e81fb6315add8f538fdb917fed000507ce90672d
Kriner <bkriner@mocs.flsouthern.edu>
2025-03-13 20:29:02 -0400

    added change profile button on settings page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from. import views
 from .views import home
 
 app_name = "home"
 
 urlpatterns = [
     path("", home, name="home"),  # This will be your new home route
+    path('account/', views.account_view, name='account'),
 ]
