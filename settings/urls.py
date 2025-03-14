d217112f56fc8361341a470203ebb120e8bae939
knol <knchrist1@icloud.com>
2025-03-14 13:50:07 -0400

    had to make slight fix to main branch in order to save correctly

--- settings/urls.py
+++ settings/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import account_page, setting, update_user  
+from .views import setting, update_user  
 
 app_name = "settings"
 
 urlpatterns = [
     path('', setting, name='setting'),
-    path('account/', account_page, name='account'),
     path('account/update/', update_user, name='update_user'),  
 ]
--- settings/views.py
+++ settings/views.py
@@ -2,14 +2,10 @@ from django.shortcuts import render
 
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def setting(request):
     return render(request, "settings/settings.html")  # Ensure this template exists in home/templates/home/
 
-
-
-@login_required
-def account_page(request):
-    return render(request, 'account.html', {'user': request.user})
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
