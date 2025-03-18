b003971ce8b62b7a3538a9697b8a15078d174a50
mmulvihill13 <madisonmulvihill09@gmail.com>
2025-03-18 09:58:30 -0400

    Styled the menu, and linked it to the detailed menu

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import menu
+from . import views
 
 app_name = "order"  
 
 urlpatterns = [
     path('menu/', menu, name='menu'),
+    path('detailed-menu/<str:drink_name>/', views.detailed_menu, name='detailed_menu'),
 ]
--- order/views.py
+++ order/views.py
@@ -6,3 +6,6 @@ def menu(request):
     hotDrinks = Drink.objects.filter(in_stock=True).filter(category__name='Hot Drinks')
     
     return render(request, 'order/menu.html',  {'hotDrinks': hotDrinks , 'coldDrinks': coldDrinks})
+
+def detailed_menu(request, drink_name):
+    return render(request, 'order/detailed_menu.html', {'drink_name': drink_name})
