bf091af5e9856ae57738a6a5269d45fe517b6559
ana-gilbert <152214588+ana-gilbert@users.noreply.github.com>
2025-03-26 17:31:06 -0400

    small migrations changes

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -15,6 +15,8 @@ import dj_database_url
 import os
 from dotenv import load_dotenv
 load_dotenv()
+print(os.getenv('DATABASE_URL'))  # Debug line to confirm loading
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
