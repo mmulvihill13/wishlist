de2ce328f717a3f53b2551abac335b452008e206
Kriner <bkriner@mocs.flsouthern.edu>
2025-04-17 15:49:06 -0400

    reviews tab created and appears now

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Review  # Import the Review model
 
 @login_required
 def home(request):
-    return render(request, "home/home.html")  # Ensure this template exists in home/templates/home/
+    recent_reviews = Review.objects.order_by('-created_at')[:3]
+    return render(request, "home/home.html", {'recent_reviews': recent_reviews})
 
 def account_view(request):
     return render(request, 'home/account.html')
+
+def all_reviews(request):
+    reviews = Review.objects.all()
+    return render(request, 'home/all_reviews.html', {'reviews': reviews})
