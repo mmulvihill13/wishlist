356a62aac7ad0e0ec45f7e815d9df5456a67d65f
mmulvihill13 <madisonmulvihill09@gmail.com>
2025-04-19 12:20:05 -0400

    Make the rewards work for checkout and actually take off the lowest cost drink from the total!

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages 
 from cart.cart import Cart 
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 #checkout function
+@login_required
 def checkout(request):
     cart = Cart(request)
     # print("Sessino ID: ", request.session.session_key)
     # print("Cart data:", cart)
     cart_drinks = []
     cart_price = 0
+    profile = request.user.profile
+    free_drinks = profile.free_drinks_avail
+
 
     #if there i something in the cart 
     if len(cart) >0 :
@@ -38,11 +42,24 @@ def checkout(request):
             'price': price,
             'id': drink_id,
             'total_price': total_price,
-        })
+            })
         
-        #add it to the total cart price
-        cart_price += total_price 
-    
+            #add it to the total cart price
+            cart_price += total_price 
+        lowest_drink = min(item['price'] for item in cart_drinks)
+        
+        discount_applied = False
+        discount_amount = 0
+        free_drink_name = None
+
+        if free_drinks > 0:
+            cart_price -= lowest_drink
+            profile.free_drinks_avail -= 1
+            profile.save()
+            discount_applied = True
+            discount_amount = lowest_drink
+            free_drink_name = next((item['drink'].name for item in cart_drinks if item['price'] == lowest_drink), None)
+
     locations = [
         {"name": "Becker", "image_url": "https://th.bing.com/th/id/R.621fed752366a5815416b0c987ab10b9?rik=bCXdKnGyi51%2fgQ&pid=ImgRaw&r=0", "estimated_delivery_time": "10 minutes"},
         {"name": "Cafe", "image_url": "https://i.pinimg.com/736x/a2/bc/9d/a2bc9d4d9afd8dd98f65e30d349bfee4.jpg", "estimated_delivery_time": "20 minutes"},
@@ -53,7 +70,11 @@ def checkout(request):
     context = {
         'cart_drinks': cart_drinks,
         'cart_price': cart_price,
-        'locations': locations
+        'locations': locations, 
+        'free_drinks': profile.free_drinks_avail,
+        'discount_applied': discount_applied,
+        'discount_amount': discount_amount,
+        'free_drink_name': free_drink_name,
     }
     return render(request, 'checkout/checkout.html', context)
 
@@ -71,6 +92,7 @@ def ordered(request):
             #let the user know if they got a free drink
             if profile.order_count%10 ==0:
                 profile.free_drinks_avail += 1 
+                profile.order_count =0
                 messages.success(request, 'You earned a free drink!')
         profile.save()
         messages.success(request, 'You order has been placed successfully')
